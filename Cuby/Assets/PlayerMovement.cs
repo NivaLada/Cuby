b954623b448e24250f86a1ba01a8c5fabb59da8f
NivaLada <69445722+NivaLada@users.noreply.github.com>
2021-02-12 17:50:47 +0100

    Cuby V0.01

---
+++ Cuby/Assets/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public CharacterController2D controller;
+
+
+    public float sidemovespeed = 40f;
+    float sidemove = 100f;
+    bool jump = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log( sidemove = Input.GetAxisRaw("Horizontal") * sidemovespeed); //General Movement l,r
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(sidemove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
