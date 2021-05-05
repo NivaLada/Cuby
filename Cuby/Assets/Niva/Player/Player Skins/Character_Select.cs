2b6291dd171893def528117dd46e9073bfb16c87
STA-ZKAlex2607 <stazkalex2607@gmail.com>
2021-05-05 12:07:27 +0200

    Character Select
    skin auswählbar

---
+++ Cuby/Assets/Niva/Player/Player Skins/Character_Select.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class Character_Select : MonoBehaviour
+{
+
+
+    public GameObject player;
+    public Sprite skin;
+
+
+    public void Cube() 
+    {
+
+
+
+       
+            player.GetComponent<SpriteRenderer>().sprite = skin;
+    }
+    
+}
